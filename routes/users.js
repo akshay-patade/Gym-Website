029172976eef76cc8d75cd293f244dd0f0c051b7
akshay-patade <akshaypatade@gmail.com>
2022-12-02 12:57:03 -0500

    Created a new route users.js

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+//const index = require("../data");
+//const people = index.people;
+
+router.route("/").get(async (req, res) => {
+    //code here for GET
+    res.status(200).json({ error: "Website is working" });
+  });
+
+
+module.exports = router
