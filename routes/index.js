f7673395090856334a675b404c9e598bfed4fb2f
itsshuchimehta <115120084+itsshuchimehta@users.noreply.github.com>
2022-12-02 16:12:02 -0500

    created collections,connection and setting.json

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const users = require("./users");
+const admin = require("./admin");
 
 const constructorMethod = (app) => {
-  
-    app.use("/", users);
-    app.use("*", (req, res) => {
-      res.status(404).json({ error: "Not found" });
-    });
-  };
-  
-  module.exports = constructorMethod;
+  app.use("/", users);
+  app.use("/admin", admin);
+
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" });
+  });
+};
+
+module.exports = constructorMethod;
