74ebe6ffa4a880b21470e8c2ae6b405b43c9aeb8
akshay-patade <akshaypatade@gmail.com>
2022-12-12 18:12:44 -0500

    Fixed the ajax code

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,8 +2,8 @@
 
 var products = [];
 var filterProducts = [];
-
 (function ($) {
+
   "use strict";
 
   $(function () {
@@ -12,8 +12,8 @@ var filterProducts = [];
 
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    var box = $(".header-text").height();
-    var header = $("header").height();
+    var box = $('.header-text').height();
+    var header = $('header').height();
 
     if (scroll >= box - header) {
       $("header").addClass("background-header");
@@ -22,69 +22,65 @@ var filterProducts = [];
     }
   });
 
-  $(".schedule-filter li").on("click", function () {
-    var tsfilter = $(this).data("tsfilter");
-    $(".schedule-filter li").removeClass("active");
-    $(this).addClass("active");
-    if (tsfilter == "all") {
-      $(".schedule-table").removeClass("filtering");
-      $(".ts-item").removeClass("show");
+
+  $('.schedule-filter li').on('click', function () {
+    var tsfilter = $(this).data('tsfilter');
+    $('.schedule-filter li').removeClass('active');
+    $(this).addClass('active');
+    if (tsfilter == 'all') {
+      $('.schedule-table').removeClass('filtering');
+      $('.ts-item').removeClass('show');
     } else {
-      $(".schedule-table").addClass("filtering");
+      $('.schedule-table').addClass('filtering');
     }
-    $(".ts-item").each(function () {
-      $(this).removeClass("show");
-      if ($(this).data("tsmeta") == tsfilter) {
-        $(this).addClass("show");
+    $('.ts-item').each(function () {
+      $(this).removeClass('show');
+      if ($(this).data('tsmeta') == tsfilter) {
+        $(this).addClass('show');
       }
     });
   });
 
+
   // Window Resize Mobile Menu Fix
   mobileNav();
+
 
   // Scroll animation init
   window.sr = new scrollReveal();
 
+
   // Menu Dropdown Toggle
-  if ($(".menu-trigger").length) {
-    $(".menu-trigger").on("click", function () {
-      $(this).toggleClass("active");
-      $(".header-area .nav").slideToggle(200);
+  if ($('.menu-trigger').length) {
+    $(".menu-trigger").on('click', function () {
+      $(this).toggleClass('active');
+      $('.header-area .nav').slideToggle(200);
     });
   }
 
 
-
-
   $(document).ready(function () {
+    $(document).on("scroll", onScroll);
 
     //smoothscroll
-    $('.scroll-to-section a[href^="#"]').on("click", function (e) {
+    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
       e.preventDefault();
       $(document).off("scroll");
 
-      $("a").each(function () {
-        $(this).removeClass("active");
-      });
-      $(this).addClass("active");
+      $('a').each(function () {
+        $(this).removeClass('active');
+      })
+      $(this).addClass('active');
 
       var target = this.hash,
         menu = target;
       var target = $(this.hash);
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: target.offset().top + 1,
-          },
-          500,
-          "swing",
-          function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-          }
-        );
+      $('html, body').stop().animate({
+        scrollTop: (target.offset().top) + 1
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+      });
     });
 
 
@@ -103,28 +99,49 @@ var filterProducts = [];
     )
   });
 
+  function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        $('.nav ul li a').removeClass("active");
+        currLink.addClass("active");
+      }
+      else {
+        currLink.removeClass("active");
+      }
+    });
+  }
+
+
   // Page loading animation
-  $(window).on("load", function () {
-    $("#js-preloader").addClass("loaded");
+  $(window).on('load', function () {
+
+    $('#js-preloader').addClass('loaded');
+
   });
 
+
   // Window Resize Mobile Menu Fix
-  $(window).on("resize", function () {
+  $(window).on('resize', function () {
     mobileNav();
   });
+
 
   // Window Resize Mobile Menu Fix
   function mobileNav() {
     var width = $(window).width();
-    $(".submenu").on("click", function () {
+    $('.submenu').on('click', function () {
       if (width < 767) {
-        $(".submenu ul").removeClass("active");
-        $(this).find("ul").toggleClass("active");
+        $('.submenu ul').removeClass('active');
+        $(this).find('ul').toggleClass('active');
       }
     });
   }
-})(window.jQuery);
 
+
+})(window.jQuery);
 
 //Using fetchapi to get all the products
 function showProducts(products) {
@@ -227,5 +244,3 @@ if (document.querySelector('input[name="filter_by_price"]')) {
     });
   });
 }
-
-
