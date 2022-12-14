660e2f8305f55579826b68b574a14ab6e35b4ead
itsshuchimehta <115120084+itsshuchimehta@users.noreply.github.com>
2022-12-13 19:24:21 -0500

    email validations

--- public/js/custom.js
+++ public/js/custom.js
@@ -59,7 +59,6 @@ var filterProducts = [];
 
   $(".emailID").on("change", function () {
     var email = $(this).val();
-    // var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var mailformat =
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (email.match(mailformat)) {
