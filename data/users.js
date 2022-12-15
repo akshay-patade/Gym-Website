73441e457a89f78f2c888ce521e3955c3b80a9ef
itsshuchimehta <115120084+itsshuchimehta@users.noreply.github.com>
2022-12-14 22:45:35 -0500

    New user Registration with validations, usre already Exist, Error handlebar and success handlebar

--- data/users.js
+++ data/users.js
@@ -1,9 +1,63 @@
 const mongoCollections = require("../config/mongoCollections");
 const user = mongoCollections.user;
 const { ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
+const saltRounds = 16;
+
+//Register User
+const createUser = async (
+  firstname,
+  lastname,
+  gender,
+  dob,
+  address,
+  zipcode,
+  cell,
+  email,
+  password
+) => {
+  let insertStatus = {};
+  email = email.toLowerCase();
+  const userCollection = await user();
+  const FoundUser = await userCollection.findOne({
+    email: email,
+  });
+
+  if (FoundUser !== null) throw "Username Already Exist!";
+  //     insertStatus.insertedUser = false;
+  //     insertStatus.alreadyExist = true;
+  //     return insertStatus;
+  //   }
+
+  const hashPassword = await bcrypt.hash(password, saltRounds);
+
+  let new_user = {
+    firstname: firstname,
+    lastname: lastname,
+    gender: gender,
+    dob: dob,
+    address: address,
+    zipcode: zipcode,
+    cell: cell,
+    email: email,
+    password: hashPassword,
+  };
+  const insertInfo = await userCollection.insertOne(new_user);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+    // insertStatus.insertedUser = false;
+    // insertStatus.alreadyExist = false;
+    // insertStatus.otherErr = true;
+    // return insertStatus;
+    throw "Could not insert";
+  }
+
+  insertStatus.insertedUser = true;
+  //   insertStatus.alreadyExist = false;
+
+  return insertStatus;
+};
 
 //Get the User by Email Id
-
 const getUserByEmail = async (emailID) => {
   //Code to check the parameters of the emailID
   if (!emailID) throw "Please Provide email ID";
@@ -25,5 +79,6 @@ const getUserByEmail = async (emailID) => {
   return userbyemail;
 };
 module.exports = {
+  createUser,
   getUserByEmail,
 };
--- routes/users.js
+++ routes/users.js
@@ -33,14 +33,104 @@ router.route("/login").get(async (req, res) => {
   });
 });
 
-router.route("/register").get(async (req, res) => {
-  //code here for GET
-  res.status(200).render("register", {
-    title: "Register",
-    user_header: true,
-    user_footer: true,
+router
+  .route("/register")
+  .get(async (req, res) => {
+    //code here for GET
+    res.status(200).render("register", {
+      title: "Register",
+      user_header: true,
+      user_footer: true,
+    });
+  })
+  .post(async (req, res) => {
+    const userPostData = req.body;
+
+    // try {
+    //   if (!userPostData.usernameInput || !userPostData.passwordInput)
+    //     throw "Username and Password Must be supplied";
+    //   validation.CheckUsername(userPostData.usernameInput);
+    //   validation.CheckPassword(userPostData.passwordInput);
+    // } catch (e) {
+    //   res.status(400).render("userRegister", {
+    //     error: e,
+    //     hasErrors: true,
+    //     title: "User Register",
+    //   });
+    //   return;
+    // }
+    e = "Internal Server Error";
+    // res.status(500).json({ error: e });
+    res.status(500).render("Error", {
+      hasErrors: true,
+      error: e,
+      title: "Error",
+      user_header: true,
+      user_footer: true,
+    });
+
+    return;
+    // try {
+    //   userPostData.firstname = userPostData.firstname.trim();
+    //   userPostData.lastname = userPostData.lastname.trim();
+    //   userPostData.address = userPostData.address.trim();
+    //   userPostData.email = userPostData.email.trim();
+
+    //   const {
+    //     firstname,
+    //     lastname,
+    //     gender,
+    //     dob,
+    //     address,
+    //     zipcode,
+    //     cell,
+    //     email,
+    //     password,
+    //   } = userPostData;
+
+    //   const ResultStatus = await users.createUser(
+    //     firstname,
+    //     lastname,
+    //     gender,
+    //     dob,
+    //     address,
+    //     zipcode,
+    //     cell,
+    //     email,
+    //     password
+    //   );
+
+    //   if (ResultStatus.insertedUser === true) {
+    //     // res.redirect("/");
+    //     res.status(200).render("userRegisterSuccess", {
+    //       hasErrors: false,
+    //       title: "User Registered Successfully",
+    //       user_header: true,
+    //       user_footer: true,
+    //     });
+    //   }
+    // } catch (e) {
+    //   if (e == "Username Already Exist!") {
+    //     res.status(400).render("register", {
+    //       AlreadyExist: true,
+    //       title: "Register",
+    //       user_header: true,
+    //       user_footer: true,
+    //     });
+    //   } else {
+    //     e = "Internal Server Error";
+    //     // res.status(500).json({ error: e });
+    //     res.status(500).render("Error", {
+    //       hasErrors: true,
+    //       error: e,
+    //       title: "Error",
+    //       user_header: true,
+    //       user_footer: true,
+    //     });
+    //   }
+    //   return;
+    // }
   });
-});
 
 router
   .route("/forgotPassword")
