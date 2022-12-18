c4219eee5f61354fdcf77a2ee8e8191764ba978f
akshay-patade <akshaypatade@gmail.com>
2022-12-18 02:46:35 -0500

    Created memberSubscription

--- data/index.js
+++ data/index.js
@@ -2,10 +2,14 @@ const productData = require("./products");
 const blogData = require("./blogs");
 const userData = require("./users");
 const subscriptionPlansData = require("./subscriptionsPlans");
+const memberSubscriptionsData = require("./memberSubscription");
+const memberData = require("./member");
 
 module.exports = {
   products: productData,
   blogs: blogData,
   users: userData,
-  subscriptions: subscriptionPlansData
+  subscriptions: subscriptionPlansData,
+  memberSubscriptions: memberSubscriptionsData,
+  members: memberData
 };
