d2f5701d5fb3ed1fdd0f7fcb75f9bee3b165cb38
itsshuchimehta <115120084+itsshuchimehta@users.noreply.github.com>
2022-12-18 00:30:13 -0500

    Cart implementation cart detail page, validation, ajax for remove from cart

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ const api = require("./api");
 const admin = require("./admin");
 const blogs = require("./blogs");
 const products = require("./products");
-<<<<<<< HEAD
 const cart = require("./cart");
-=======
 const subscriptions = require("./subscriptions");
->>>>>>> 90e4f8950f8960b9a59e4567e1e6b3a53564e8ab
 const path = require("path");
 
 const constructorMethod = (app) => {
@@ -16,11 +13,8 @@ const constructorMethod = (app) => {
   app.use("/admin", admin);
   app.use("/blogs", blogs);
   app.use("/products", products);
-<<<<<<< HEAD
   app.use("/cart", cart);
-=======
   app.use("/subscriptions", subscriptions);
->>>>>>> 90e4f8950f8960b9a59e4567e1e6b3a53564e8ab
 
   app.use("*", (req, res) => {
     res.redirect("/404");
